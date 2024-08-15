394fa1244d55075d57cd79996c1c81180d0d7950
YezidBlanco <yezidblanco@gmail.com>
2024-08-15 16:44:46 -0500

    Actualizacion urls, views y dashboard

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,11 +5,11 @@ from myapp import views
 urlpatterns = [
     path('', views.index),
     path('list_categorias/', views.list_categorias),
-
     path('list_categorias/<int:id>', views.detail_categoria),
     path('list_productos/', views.list_productos),
     path('list_productos/<int:id>', views.detail_productos),
     path('list_productos/<int:id>/delete', views.delete_productos),
     path('list_productos/<int:id>/update', views.update_productos),
     path('dashboard', views.dashboard),
+    path('productos_por_vencer', views.prodcuts_por_vencer),
 ]
